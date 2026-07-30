bcb0f3b496322b4f98b02d0b389385a7e1f182ba
Sakif Istiak <istiaksakif@gmail.com>
2026-07-30 19:55:37 +0600

    Header Fotter logo dynamic from admin panel dark and light both

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\View;
+use App\Models\SiteSetting;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +21,14 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Share site settings (logo, name) with ALL frontend blade views
+        View::composer('pages.*', function ($view) {
+            $view->with([
+                'siteLogo'      => SiteSetting::getValue('logo_light', '/media/b3ca13-kg-lockup-v2.png'),
+                'siteLogoDark'  => SiteSetting::getValue('logo_dark',  '/media/b3ca13-kg-lockup-v2.png'),
+                'siteName'      => SiteSetting::getValue('site_name',  'Khan Gadget'),
+                'siteSlogan'    => SiteSetting::getValue('site_slogan', 'Eternal Tech Companion'),
+            ]);
+        });
     }
 }
